e6dea3d2d11b965f73342c0045ea83e70f5982b6
Caroline Shields <caroline.shields@amkor.com>
2017-12-10 17:20:25 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,11 +54,10 @@ class InteractiveRecord
   end
 
   def self.find_by(attributes)
-    
+
       binding.pry
       sql = "SELECT * FROM #{self.table_name} WHERE #{attributes.keys.first} = '#{value}'"
       DB[:conn].execute(sql)
-    end
   end
 
 end
